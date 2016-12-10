2d99fb53686c5945d962b98d0739afa81075fa94
Pekka Latsa <pekka.latsa@outlook.com>
2016-12-10 21:30:28 +0200

    lisätään foorumi herokuun

--- src/main/java/wepaForum/config/ProductionProfile.java
+++ src/main/java/wepaForum/config/ProductionProfile.java
@@ -2,28 +2,14 @@ package wepaForum.config;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import javax.annotation.PostConstruct;
 import org.apache.commons.dbcp2.BasicDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import wepaForum.domain.Forum;
-import wepaForum.repository.ForumRepository;
 
 @Configuration
 @Profile("production")
 public class ProductionProfile {
-    @Autowired
-    ForumRepository forumRepository;
-    
-    @PostConstruct
-    public void init() {
-        if (forumRepository.findAll().isEmpty()) {
-            Forum forum = new Forum("wepa-Forum");
-            forumRepository.save(forum);
-        }
-    }
 
     @Bean
     public BasicDataSource dataSource() throws URISyntaxException {
--- src/main/java/wepaForum/controller/ForumController.java
+++ src/main/java/wepaForum/controller/ForumController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import wepaForum.domain.ForumCategory;
 import wepaForum.domain.Message;
 import wepaForum.domain.SubForum;
